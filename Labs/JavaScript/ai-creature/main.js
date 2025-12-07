7d6602dd9611a7f6251d31edc16c34a0ce1bc1fa
Bob-Gruen <162261054+Bob-Gruen@users.noreply.github.com>
2025-12-07 16:58:06 -0500

    Delete Labs/JavaScript/ai-creature directory

--- Labs/JavaScript/ai-creature/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const canvas = document.getElementById('canvas1');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    const simulation = new Simulation(canvas);
-    simulation.init();
-});
